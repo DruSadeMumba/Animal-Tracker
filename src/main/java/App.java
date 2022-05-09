174b978917c42c26d7299f2a374bb7ad479f489c
DruSadeMumba <drusademumba@gmail.com>
2022-05-09 16:00:41 +0300

    Add layouts to all the handlebars pages

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,9 +12,29 @@ public class App {
         port(2345);
         Map<String, Object> model = new HashMap<String, Object>();
 
+                //main page
         get("/", (request, response)
             -> new ModelAndView(model, "index.hbs"),
             new HandlebarsTemplateEngine());
 
+                //sightings page
+        get("/sightings", (request, response)
+            -> new ModelAndView(model, "sightings.hbs"),
+            new HandlebarsTemplateEngine());
+
+                //new sighting
+        get("/sighting/new", (request, response)
+            -> new ModelAndView(model, "sighting-form.hbs"),
+            new HandlebarsTemplateEngine());
+
+                //animals page
+        get("/animals", (request, response)
+            -> new ModelAndView(model, "animals.hbs"),
+            new HandlebarsTemplateEngine());
+
+                //new animal
+        get("/animal/new", (request, response)
+            -> new ModelAndView(model, "animal-form.hbs"),
+            new HandlebarsTemplateEngine());
     }
 }
